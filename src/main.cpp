e5f09bd577726e5870e3617c5f56e7fec181f778
Riddhima06 <riddhima.rajput_cs.aiml24@gla.ac.in>
2026-02-21 14:15:29 +0530

    implement basic MG995 servo sweep from 0° to 180° and back

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
+/**
+* @file main.cpp
+ * @brief MG995 Closed-Loop Axis Control System
+ * @author Riddhima Rajput
+ * @date 2026-02-20
+ *
+ * @details
+ * This program implements PWM-based control of MG995 servo motor.
+ * The system accepts angle input via Serial Monitor and moves
+ * the servo after validating input range (0–180 degrees).
+ */
 #include <Arduino.h>
+#include <Servo.h>
+Servo myServo;
 void setup() {
-// write your initialization code here
-}
 
 
-void loop() {
-// write your code here
+
+
 }
